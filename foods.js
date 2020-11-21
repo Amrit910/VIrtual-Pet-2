eb768ab88a4c0f03b510aeda7e016ed56910f147
Amrit910 <72169499+Amrit910@users.noreply.github.com>
2020-11-21 18:40:47 -0500

    Add files via upload

---
+++ foods.js
@@ -0,0 +1,6 @@
+class Foods{
+    constructor(x,y,w,h){
+        var foodStock,lastFed;
+        var getFoodStock,getFoodStock,deductFoodStock;
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,32 @@
+var dog;
+var database;
+var foods,foodstock;
+var milk;
+
+function preload()
+{
+  dog = loadImage("dogImg.png");
+  happydog = loadImage("dongImg1.png");
+}
+
+function setup() {
+	createCanvas(500,500);
+  
+}
+
+
+function draw() {  
+  var backGround()
+
+  drawSprites();
+  //add styles here
+
+}
+
+dog.display();
+foods.display();
+foodstock.display();
+happydog.display();
+milk.display();
+
+
